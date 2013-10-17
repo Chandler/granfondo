c6b6e67e54a50d04e4947f9910955743a57738f6
Chandler Abraham <cabraham@twitter.com>
2013-10-16 19:02:41 -0700

    cleaning up and styling

--- chart.js
+++ chart.js
@@ -1,10 +1,124 @@
 $(document).ready(function () {
-  var json, map,
-  path, vis, xy,
-  duration, offset, origin_x, origin_y, len, group, circle;
+  var width  = 1100;
+  var height = 800;
+  var json;
+  var group;
+  //var highlighted = ["5084","5501","5857","3237","2996"];
+  var highlighted = [];
 
-  var width  = 900;
-  var height = 900;
+  var ROUTES = [
+    "gran", 
+    "gran_wc", 
+    "medio",
+    "medio_wc",
+    "piccolo"]
+  
+  var ROUTE_COLORS = {
+    "gran":     "#38383A",
+    "gran_wc":  "#38383A",
+    "medio":    "#DD610E",
+    "medio_wc": "#DD610E",
+    "piccolo":  "#850C29"
+  }
+
+  $('.restart').on('click', function(){
+    drawCircles();
+  })
+
+
+  d3.json("data.json", function(json) {
+    this.json  = json;
+    var path   = autoScaledProjection(json);
+    var riders = json.riders;
+
+    vis = d3.select("#map")
+      .append("svg:svg")
+      .attr("width", 1500)
+      .attr("height", 1000);
+
+    //draw paths
+    for(var i = 0; i < json.features.length; i++) {
+      var name = json.features[i].properties.name
+      vis.append("svg:g")
+        .attr("class", name+"_route")
+        .selectAll("path")
+        .data([json.features[i]])
+        .enter()
+        .append("svg:path")
+        .attr("d", path)
+        .attr("fill-opacity", 0.5)
+        .attr("fill", "#fff")
+        .attr("stroke", "#333");
+    }
+
+    group = vis.append("svg:g");
+    drawCircles();
+  });
+
+
+  var drawCircles = function() {
+    d3.selectAll('circle').remove();
+    var pathNodes    = {};
+    var deduplicater = {};
+    for(var i = 0; i < ROUTES.length; i++) {
+      var route =  ROUTES[i];
+      var riders = this.json[route];
+      
+      if(!deduplicater[route]){
+        deduplicater[route] = []
+      } 
+
+      for(var j =0; j < riders.length; j++) {
+        var radius;
+        var bib           = riders[j]["bib"];
+        var speed         = riders[j]["wall_clock_seconds"];
+        var rounded_speed = Math.floor((speed/80))*80; //round to nearest 100 seconds for grouping
+        
+        
+        //Always draw a rider who is being highlighted
+        if(highlighted.indexOf(bib) > -1) {
+          radius = 8;
+        } else {
+          //one draw one circle for each distinct rounded_speed per route
+          if (deduplicater[route][rounded_speed]){
+            continue;
+          }else {
+            deduplicater[route][rounded_speed] = true;
+          }
+          radius = 2;
+        }
+
+        //lazy initialize pathnodes for each route
+        if(!pathNodes[route]) {
+          var targetPath = d3.selectAll('.' + route + '_route')[0][0];
+          pathNodes[route] = d3.select(targetPath).selectAll('path').node();
+        }
+
+        var circle = group.append("circle")
+          .attr({
+          r: radius,
+          fill: ROUTE_COLORS[route],
+          route: route,
+          transform: function () {
+              var nodes = pathNodes[this.getAttribute('route')];
+              var p = nodes.getPointAtLength(0)
+              return "translate(" + [p.x, p.y] + ")";
+          }
+        });
+
+         circle.transition()
+          .duration(rounded_speed*2)
+          .ease("linear")
+          .attrTween("transform", function (d, index) {
+            var nodes = pathNodes[this.getAttribute('route')];
+            return function (t) {
+              var p = nodes.getPointAtLength(nodes.getTotalLength()*t);
+              return "translate(" + [p.x, p.y] + ")";
+            }
+        }); 
+      }
+    } 
+  }
 
   //http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
   var autoScaledProjection = function(json) {
@@ -31,113 +145,4 @@ $(document).ready(function () {
       .scale(scale).translate(offset);
     return path.projection(projection);d
   }
-
-  d3.json("data.json", function(json) {
-    var routes = ["gran", "gran_wc", "medio", "medio_wc","piccolo"]
-    // var routes = ["gran_wc","medio_wc"]
-    var colors = {
-      "gran": "#38383A",
-      "gran_wc": "#38383A",
-      "medio": "#DD610E",
-      "medio_wc": "#DD610E",
-      "piccolo": "#850C29"
-    }
-    var path = autoScaledProjection(json);
-    var riders = json.riders
-
-    vis = d3.select("#map")
-      .append("svg:svg")
-      .attr("width", 1500)
-      .attr("height", 1100);
-
-    //draw paths
-    var i;
-    for(i =0; i < json.features.length; i++) {
-      var name = json.features[i].properties.name
-      vis.append("svg:g")
-        .attr("class", name+"_route")
-        .selectAll("path")
-        .data([json.features[i]])
-        .enter()
-        .append("svg:path")
-        .attr("d", path)
-        .attr("fill-opacity", 0.5)
-        .attr("fill", "#fff")
-        .attr("stroke", "#333");
-    }
-
-    group = vis.append("svg:g");
-
-    var pathNodes = function(route) {
-      var targetPath = d3.selectAll('.' + route + '_route')[0][0];
-      return d3.select(targetPath).selectAll('path').node();
-    }
-
-    // Draw circles
-    var pathNodes = {}
-    var deduplicater = {}
-    var count = 0; 
-    for(i =0; i < routes.length; i++) {
-
-      var route = routes[i];
-      var riders = json[route]
-      
-      if(!deduplicater[route]){
-        deduplicater[route] = []
-      } 
-      
-      var j;
-      for(j =0; j < riders.length; j++) {
-        var bib = riders[j]["bib"];
-        var speed = riders[j]["wall_clock_seconds"];
-        var rounded_speed = Math.floor((speed/100))*100
-        
-
-        var radius;
-        if(["5084","5501","5857","3237","2996"].indexOf(bib) > -1) {
-          radius = 8;
-        }else {
-          if (deduplicater[route][rounded_speed]){
-            //dont draw this rider
-            continue;
-          }else {
-            deduplicater[route][rounded_speed] = true;
-            count = count+1;       
-          }
-          radius = 2;
-        }
-
-
-        //lazy load
-        if(!pathNodes[route]){
-          var targetPath = d3.selectAll('.' + route + '_route')[0][0];
-          pathNodes[route] = d3.select(targetPath).selectAll('path').node();
-        }
-        if(radius == 3) {console.log("ok")}
-        var circle = group.append("circle")
-          .attr({
-          r: radius,
-          fill: colors[route],
-          route: route,
-          transform: function () {
-              var nodes = pathNodes[this.getAttribute('route')];
-              var p = nodes.getPointAtLength(0)
-              return "translate(" + [p.x, p.y] + ")";
-          }
-        });
-
-         circle.transition()
-          .duration(rounded_speed*2)
-          .ease("linear")
-          .attrTween("transform", function (d, index) {
-            var nodes = pathNodes[this.getAttribute('route')];
-            return function (t) {
-              var p = nodes.getPointAtLength(nodes.getTotalLength()*t);
-              return "translate(" + [p.x, p.y] + ")";
-            }
-        }); 
-      }
-    } 
-    console.log(count)
-  });
 });
